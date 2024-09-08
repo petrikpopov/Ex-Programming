ff3497b43bb14a2b0d48b568fa8ae1bf0fcbc319
Popo_tz39@student.itstep.org <Popo_tz39@student.itstep.org>
2024-09-08 21:35:21 +0300

    Добавьте файлы проекта.

---
+++ APP/RomanNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP
+{
+    public class RomanNumber
+    {
+        private readonly int _value;
+
+        public RomanNumber(int value)
+        {
+            _value = value;
+        }
+
+        public int Value { get { return _value; } }
+
+        public static RomanNumber Parse(string value)
+        {
+            int result = 0;
+            int prevDigit = 0;
+
+            foreach (char c in value.Reverse())
+            {
+                int digit = DigitValue(c);
+                result += digit < prevDigit ? -digit : digit;
+                prevDigit = digit;
+            }
+
+            return new RomanNumber(result);
+        }
+
+        public static int DigitValue(char digit)
+        {
+            switch (digit)
+            {
+                case 'N': return 0;
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                default: return 1000;
+            }
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using APP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RomanNumberTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RomanNumber rn = RomanNumber.Parse("");
+            rn = null;
+            Assert.IsNotNull(rn, "Parse result is not null!");
+            Assert.AreEqual(2, rn.Value, "Zero testing");
+        }
+
+        [TestMethod]
+        public void ParseTest()
+        {
+            Dictionary<String, int> testCases = new Dictionary<String, int>()
+            {
+                { "N", 0},
+                { "I", 1},
+                { "II", 2},
+                { "III", 3},
+                { "IIII", 4},
+                { "V", 5},
+                { "X", 10},
+                { "D", 500},
+                { "IV", 4},
+                { "VI", 6},
+                { "XI", 11},
+                { "IX", 9},
+                { "MM", 2000},
+                { "MCM", 1900},
+                //HM
+                { "XL", 40},
+                { "XC", 90},
+                { "CD", 400},
+                { "CMII", 902},
+                { "DCCCC", 900},
+                { "CCCC", 400},
+                { "XIXIIII", 23},
+                { "XXXXX", 50},
+                { "DDMD", 1500},
+
+
+            };
+            foreach (var testCase in testCases)
+            {
+                RomanNumber rn = RomanNumber.Parse(testCase.Key);
+                Assert.IsNotNull(rn, $"Parse result of '{testCase.Key}' is not null");
+                Assert.AreEqual(
+                    testCase.Value,
+                    rn.Value,
+                    $"Parse '{testCase.Key}' => {testCase.Value}"
+                    );
+            }
+        }
+
+        [TestMethod]
+        public void DigitValueChar()
+        {
+            Dictionary<char, int> testCases = new Dictionary<char, int>()
+            {
+                {'N', 0},
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(testCase.Value,
+                    RomanNumber.DigitValue(testCase.Key),
+                    $"{testCase.Key} => {testCase.Value}"
+                    );
+            }
+
+            // HomeW_05.09.2024
+
+            char[] excCases = { 'N', '1', 'x', 'i', '&' };
+
+            foreach (var digit in excCases)
+            {
+                var ex = Assert.ThrowsException<ArgumentException>(
+                    () => RomanNumber.DigitValue(digit),
+                    $"Expected ArgumentException for digit: '{digit}'"
+                );
+
+                StringAssert.Contains(ex.Message, $"{digit}",
+                    $"Message should mention the invalid character '{digit}', but it was: {ex.Message}");
+                StringAssert.Contains(ex.Message, $"{nameof(RomanNumber)}",
+                    $"Message should mention the class '{nameof(RomanNumber)}' but it was: {ex.Message}");
+                StringAssert.Contains(ex.Message, $"{nameof(RomanNumber.DigitValue)}",
+                    $"Message should mention the method '{nameof(RomanNumber.DigitValue)}' but it was: {ex.Message}");
+                StringAssert.Contains(ex.Message, "argument",
+                    $"Message should contain the word 'argument', but it was: {ex.Message}");
+            }
+
+        }
+    }
+}
