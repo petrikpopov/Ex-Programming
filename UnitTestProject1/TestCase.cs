ea71c622ef12f5bf307d12956b5599d28dc9f11a
Popo_tz39@student.itstep.org <Popo_tz39@student.itstep.org>
2024-09-22 16:30:26 +0300

    Created  HomeW 19.09.2024

---
+++ UnitTestProject1/TestCase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class TestCase<TInput, TExpected>
+    {
+        public string TestName { get; set; }
+        public TInput Input { get; set; }
+        public TExpected Expected { get; set; }
+        public Action TestAction { get; set; }
+
+        public TestCase(string testName, TInput input, TExpected expected, Action testAction)
+        {
+            TestName = testName;
+            Input = input;
+            Expected = expected;
+            TestAction = testAction;
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                TestAction.Invoke();
+                Console.WriteLine($"Test '{TestName}' passed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Test '{TestName}' failed: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnifiedRomanNumberTest.cs
@@ -0,0 +1,53 @@
+﻿using APP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnifiedRomanNumberTest
+    {
+        private List<TestCase<string, int>> testCases;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            testCases = new List<TestCase<string, int>>
+            {
+                new TestCase<string, int>("Test IV", "IV", 4, () =>
+                {
+                    Assert.AreEqual(4, RomanNumberParser.Parse("IV"));
+                }),
+                new TestCase<string, int>("Test MCMXC", "MCMXC", 1990, () =>
+                {
+                    Assert.AreEqual(1990, RomanNumberParser.Parse("MCMXC"));
+                }),
+                new TestCase<string, int>("Test N", "N", 0, () =>
+                {
+                    Assert.AreEqual(0, RomanNumberParser.Parse("N"));
+                }),
+                new TestCase<string, int>("Test Invalid Characters ABC", "ABC", 0, () =>
+                {
+                    Assert.ThrowsException<ArgumentException>(() => RomanNumberParser.Parse("ABC"));
+                }),
+                new TestCase<string, int>("Test Invalid Numeral IIII", "IIII", 0, () =>
+                {
+                    Assert.ThrowsException<ArgumentException>(() => RomanNumberParser.Parse("IIII"));
+                })
+            };
+        }
+
+        [TestMethod]
+        public void RunAllTests()
+        {
+            foreach (var testCase in testCases)
+            {
+                testCase.Execute();
+            }
+        }
+    }
+}
